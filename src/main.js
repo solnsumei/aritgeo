18d2bb99d5e6ef6c0baaa4d56009b22e8d162770
Solomon <solnsumei@gmail.com>
2017-07-31 18:58:52 +0100

    Code has been implemented for non array input values

---
+++ src/main.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+    /* Check the arithmetic or geometric progression of an array */
+    aritGeo:(value)=> {
+
+        // Checking for non array values
+        if (!Array.isArray(value)) {
+            return -1;
+        }
+
+    }
+};
